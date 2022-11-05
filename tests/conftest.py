db7cc930fc899e7c94798e8754fc6a8736c6b407
mikhail-xnor <mikhail-xnor@yandex.ru>
2022-11-05 14:56:59 +0300

    Fixed environment dependencies, remove private data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
 import pytest
+import os
+import sys
+
+GMDH_ROOT = os.environ['GMDH_ROOT']
+GMDH_BINARY_FILES = os.environ['GMDH_BINARY_FILES']
+sys.path.append(GMDH_ROOT)
+sys.path.append(GMDH_BINARY_FILES)
 
 import gmdh
 
