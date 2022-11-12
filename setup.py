04e71012a815c42b3d2676a2d0af503898819c92
Artem Babin <artem031201@yandex.ru>
2022-11-12 13:59:40 +0300

    Fixed all pylint warnings

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
+"""
+A setuptools based setup module.
+"""
+
 import os
 import subprocess
 from pathlib import Path
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
-
-class CMakeExtension(Extension):
+class CMakeExtension(Extension):  # pylint: disable=too-few-public-methods, missing-class-docstring
     def __init__(self, name: str, sourcedir: str = "") -> None:
         super().__init__(name, sources=[])
         self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
-
-class CMakeBuild(build_ext):
+class CMakeBuild(build_ext):  # pylint: disable=missing-class-docstring
     def build_extension(self, ext: CMakeExtension) -> None:
 
+        cmake_args = ["-DCMAKE_BUILD_TYPE=Release", "-S", ".", "-B", "build/Release"]
         build_args = ['--config', 'Release', '--target', '_gmdh_core']
 
-        subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-S", ".", "-B", "build/Release"], check=True)
+        subprocess.run(["cmake"] + cmake_args, check=True)
         subprocess.run(["cmake", "--build", "build/Release"] + build_args, check=True)
+
+with open("README.md", "r", encoding='utf8') as f:
+    long_description = f.read()
 
 setup(
     name='gmdh',
@@ -28,7 +34,7 @@ setup(
     url='https://github.com/bauman-team/GMDH',
     license='LICENSE.md',
     description='Gmdh algorithms',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
     classifiers=[
--- tests/test_gmdh.py
+++ tests/test_gmdh.py
@@ -1,21 +1,25 @@
-import pytest
-import os
-import sys
-import numpy as np
+"""
+All tests for gmdh module.
+"""
 
+#pylint: disable=invalid-name
+
+import pytest
+import numpy as np
 import gmdh
 
-"""@pytest.fixture(scope='class')
-def sber_ts_data():
-    return pd.read_csv("Sberbank.csv")['close'][-3000:]"""
-
 class TestDataPreparations:
+    """
+    Class containing methods for testing gmdh module functions
+    that prepare user data for further use in gmdh models.
+    """
 
-    @pytest.mark.parametrize('original_data, ts_lags',
-                            [([1, 2, 3, 4, 5, 6], 1), 
-                            ([1, 2, 3, 4, 5, 6], 4),
-                            ([1, 2, 3, 4, 5, 6], 5),
-                            ([np.random.random() for i in range(10000)], 50)])
+    @pytest.mark.parametrize(
+        'original_data, ts_lags',
+        [([1, 2, 3, 4, 5, 6], 1),
+        ([1, 2, 3, 4, 5, 6], 4),
+        ([1, 2, 3, 4, 5, 6], 5),
+        ([np.random.random() for i in range(10000)], 50)])
     def test_time_series_transformation(self, original_data, ts_lags):
         """
         Testing gmdh.time_series_transformation() method using both small and large original_data.
@@ -26,32 +30,34 @@ class TestDataPreparations:
             assert np.array_equal(X[i], original_data[i:i+ts_lags])
         assert np.array_equal(y, original_data[ts_lags:])
 
-    @pytest.mark.parametrize('original_data, ts_lags',
-                            [([1, 2, 3, 4, 5, 6], 0),  # lags = 0
-                            ([1, 2, 3, 4, 5, 6], -1),  # lags < 0
-                            ([1, 2, 3, 4, 5, 6], 6),  # lags = time series size
-                            ([1, 2, 3, 4, 5, 6], 7),  # lags > time series size
-                            ([], 2)])  # empty time series
+    @pytest.mark.parametrize(
+        'original_data, ts_lags',
+        [([1, 2, 3, 4, 5, 6], 0),  # lags = 0
+        ([1, 2, 3, 4, 5, 6], -1),  # lags < 0
+        ([1, 2, 3, 4, 5, 6], 6),  # lags = time series size
+        ([1, 2, 3, 4, 5, 6], 7),  # lags > time series size
+        ([], 2)])  # empty time series
     def test_time_series_transformation_value_error(self, original_data, ts_lags):
         """
         Testing gmdh.time_series_transformation() method using incorrect input arguments values.
         Expected result is ValueError.
         """
-        with pytest.raises(ValueError) as err_info:
-            X, y = gmdh.time_series_transformation(original_data, lags=ts_lags)
+        with pytest.raises(ValueError):
+            gmdh.time_series_transformation(original_data, lags=ts_lags)
 
-    @pytest.mark.parametrize('original_data, ts_lags',
-                            [([1, 2, 3, 4, 5, 6], 3.5),  # not integer lags
-                            ([1, 2, 3, 4, 5, 6], 'a'),  # not integer lags
-                            (['a', 'b', 'c', 'd'], 3)])  # time series data contains strings
+    @pytest.mark.parametrize(
+        'original_data, ts_lags',
+        [([1, 2, 3, 4, 5, 6], 3.5),  # not integer lags
+        ([1, 2, 3, 4, 5, 6], 'a'),  # not integer lags
+        (['a', 'b', 'c', 'd'], 3)])  # time series data contains strings
     def test_time_series_transformation_type_error(self, original_data, ts_lags):
         """
         Testing gmdh.time_series_transformation() method using incorrect input arguments types.
         Expected result is TypeError.
         """
-        with pytest.raises(TypeError) as err_info:
-            X, y = gmdh.time_series_transformation(original_data, lags=ts_lags)
-    
+        with pytest.raises(TypeError):
+            gmdh.time_series_transformation(original_data, lags=ts_lags)
+
     def test_split_data(self):
         """
         Testing gmdh.split_data() method using simple X and y arrays without shuffling.
@@ -63,38 +69,38 @@ class TestDataPreparations:
         assert np.array_equal(x_test, [[7, 8, 9]])
         assert np.array_equal(y_train, [10, 11])
         assert np.array_equal(y_test, [12])
-    
+
     def test_split_data_with_shuffle(self):
         """
-        Testing gmdh.split_data() method using simple X and y arrays with shuffling and the same random_state param 2 times.
+        Testing gmdh.split_data() method using simple X and y arrays
+        with shuffling and the same random_state param 2 times.
         The expected resulting arrays for the first and second time should be equal.
         """
         X = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         y = [10, 11, 12]
-        x_train, x_test, y_train, y_test = gmdh.split_data(X, y, test_size=0.66, shuffle=True, random_state=1)
-        x_train2, x_test2, y_train2, y_test2 = gmdh.split_data(X, y, test_size=0.66, shuffle=True, random_state=1)
+        x_train, x_test, y_train, y_test = \
+            gmdh.split_data(X, y, test_size=0.66, shuffle=True, random_state=1)
+        x_train2, x_test2, y_train2, y_test2 = \
+            gmdh.split_data(X, y, test_size=0.66, shuffle=True, random_state=1)
         assert x_train.shape == (1, 3) and np.array_equal(x_train, x_train2)
         assert x_test.shape == (2, 3) and np.array_equal(x_test, x_test2)
         assert y_train.shape == (1,) and np.array_equal(y_train, y_train2)
         assert y_test.shape == (2,) and np.array_equal(y_test, y_test2)
-    
 
-    @pytest.mark.parametrize('X, y, test_size',
-                            [([], [], 0.2),  # empty arrays
-                            ([[1, 2, 3], [4, 5, 6]], [7, 8], 0.1),  # empty test arrays because of small test_size and small origin arrays
-                            ([[1, 2, 3], [4, 5, 6]], [7, 8], 0.9),  # empty train arrays because of large test_size and small origin arrays
-                            ([[1, 2, 3], [4, 5, 6]], [7], 0.5),  # different X rows and y size values
-                            ([[1, 2, 3], [4, 5, 6]], [7, 8], 0),  # test_size = 0
-                            ([[1, 2, 3], [4, 5, 6]], [7, 8], -1),  # test_size < 0
-                            ([[1, 2, 3], [4, 5, 6]], [7, 8], 1),  # test_size = 1
-                            ([[1, 2, 3], [4, 5, 6]], [7, 8], 10)])  # test_size > 1
+    @pytest.mark.parametrize(
+        'X, y, test_size',
+        [([], [], 0.2),  # empty arrays
+        ([[1, 2, 3], [4, 5, 6]], [7, 8], 0.1),  # empty test arrays because of small test_size
+        ([[1, 2, 3], [4, 5, 6]], [7, 8], 0.9),  # empty train arrays because of large test_size
+        ([[1, 2, 3], [4, 5, 6]], [7], 0.5),  # different X rows and y size values
+        ([[1, 2, 3], [4, 5, 6]], [7, 8], 0),  # test_size = 0
+        ([[1, 2, 3], [4, 5, 6]], [7, 8], -1),  # test_size < 0
+        ([[1, 2, 3], [4, 5, 6]], [7, 8], 1),  # test_size = 1
+        ([[1, 2, 3], [4, 5, 6]], [7, 8], 10)])  # test_size > 1
     def test_split_data_value_error(self, X, y, test_size):
         """
         Testing gmdh.split_data() method using incorrect input arguments values.
         Expected result is ValueError.
         """
-        with pytest.raises(ValueError) as err_info:
-            x_train, x_test, y_train, y_test = gmdh.split_data(X, y, test_size=test_size)
-
-
-
+        with pytest.raises(ValueError):
+            gmdh.split_data(X, y, test_size=test_size)
