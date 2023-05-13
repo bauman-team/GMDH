e6717060bec5ebe2c6edc339b7693fa7894af434
Artem Babin <artem031201@yandex.ru>
2023-05-13 19:24:00 +0300

    Added examples of gmdh module using

--- examples/main.cpp
+++ examples/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
     using namespace Eigen;
 
-    std::ifstream dataStream;
+    /*std::ifstream dataStream;
     std::string gmdhDir = std::getenv("GMDH_ROOT");
     dataStream.open(gmdhDir + "/examples/Sber.csv");
     std::string dataLine;
@@ -20,21 +20,24 @@ int main() {
         }
         data = Map<VectorXd, Unaligned>(dataValues.data(), dataValues.size() - 50000);
         dataStream.close();
-    }
+    }*/
 
-    //VectorXd data(10); data << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10;
-	
-    int lags = 8;
-    double validateSize = 0.2;
-    double testSize = 0.33;
-    std::cout << data.size() << std::endl;
+    VectorXd data(12); data << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12;
+    std::cout << "Original data:\n" << data << "\n\n";
+
+    int lags = 4;
     auto timeSeries = GMDH::timeSeriesTransformation(data, lags);
+    std::cout << "X data:\n" << timeSeries.first << "\n\n";
+    std::cout << "Y data:\n" << timeSeries.second << "\n\n";
+
+    double validateSize = 0.33;
     GMDH::SplittedData splittedData = GMDH::splitData(timeSeries.first, timeSeries.second, validateSize);
 
-    /*std::cout << splittedData.xTrain << "\n\n";
-    std::cout << splittedData.xTest << "\n\n";
-    std::cout << splittedData.yTrain << "\n\n";
-    std::cout << splittedData.yTest << "\n\n";*/
+    std::cout << "X train data:\n" << splittedData.xTrain << "\n\n";
+    std::cout << "X test data:\n" << splittedData.xTest << "\n\n";
+    std::cout << "Y train data:\n" << splittedData.yTrain << "\n\n";
+    std::cout << "Y test data:\n" << splittedData.yTest << "\n\n";
+    std::cout << std::endl;
 
     /*
         x1    x2     x3     x4   => x1, x2
@@ -76,33 +79,31 @@ int main() {
 
     */
 
-    //std::cout << "Original time series:\n" << data << "\n\n";
-    auto criterion = GMDH::Criterion(GMDH::CriterionType::regularity);
-    GMDH::RIA mia;
-    mia.fit(splittedData.xTrain, splittedData.yTrain, criterion, 3,
-             GMDH::PolynomialType::quadratic, testSize, 2, -1, 1, 0);
-    //GMDH::MULTI mia;
-    //mia.fit(splittedData.xTrain, splittedData.yTrain, criterion, 3, 0.5, 1, 1, 1);
-    std::cout << "\nThe best polynomial:\n\n" << mia.getBestPolynomial() << std::endl;
+    double testSize = 0.33;
+    int kBest = 10;
+    int pAverage = 1;
+    int threads = 1;
+    int verbose = 1;
+    double limit = 0;
+    auto criterion = GMDH::Criterion(GMDH::CriterionType::stability);
+    auto polynomialType = GMDH::PolynomialType::linear_cov;
 
-    VectorXd res = mia.predict(splittedData.xTest(0, all), 20);
-    mia.save("model1.txt");
+    GMDH::MIA mia;
+    mia.fit(splittedData.xTrain, splittedData.yTrain, criterion, 
+            kBest, polynomialType, testSize, pAverage, threads, verbose, limit);
 
-    if (!mia.load("model1.txt")) {
-        auto res2 = mia.predict(splittedData.xTest(0, all), 20);
+    std::cout << "\nThe best polynomial:\n" << mia.getBestPolynomial() << "\n\n";
+    
+    int lagsToPredict = 3;
+    auto res = mia.predict(splittedData.xTest(0, all), lagsToPredict);
+    std::cout << "Y predicted data:\n" << res << "\n\n";
 
-        std::cout << "\nThe best polynomial after loading:\n\n" << mia.getBestPolynomial() << std::endl;
+    mia.save("mia_model.txt");
+
+    if (!mia.load("mia_model.txt")) {
+        auto res2 = mia.predict(splittedData.xTest(0, all), lagsToPredict);
+        std::cout << "The best polynomial after model loading:\n" << mia.getBestPolynomial() << "\n\n";
+        std::cout << "Y predicted data after model loading:\n" << res2 << "\n\n";
     }
-
-    /*for (int i = 0; i < 20; ++i)
-        std::cout << splittedData.yTest[i] << " " << res[i] << " " << res2[i] << "\n";*/
-
-    //std::cout << "Predicted values before model saving:\n" << res << "\n\n";
-    //std::cout << "Predicted values after model loading:\n" << res2 << "\n\n";
-
-    //(std::cin).get();
-
-    //GMDH::COMBI combi;
-    //combi.fit(splittedData.xTrain, splittedData.yTrain, criterion, testSize, false, 0, (uint8_t)1, -123, 1);//, 0, 0, 0, -123, 1);
     return 0;
 }
